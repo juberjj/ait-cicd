0b4f32bc0b9088ec9cf6d6f17953258a232aabeb
juber-ivre <jnunes@immersivevreducation.com>
2021-04-05 11:24:15 +0100

    Adding deployment files

---
+++ afterInstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+####################################
+# SET MAIN FUNCTIONS
+####################################
+
+### Common
+#######################
+common() {
+	# Copy .env file
+	aws s3 cp s3://ait-deployment-dev/node-app/.env /usr/share/nodeapp/.env
+  
+	# Run NPM.
+	sudo npm i --prefix /usr/share/nodeapp
+}
+
+### Node Run Script
+#######################
+node_script() {
+	# Run NPM
+ cd /usr/share/nodeapp
+ npm run start
+}
+
+
+####################################
+# MAIN SCRIPT
+####################################
+# Sync app directory
+
+aws s3 cp s3://node-artifacts/app.zip /tmp/app/app.zip
+
+unzip -o /tmp/app/app.zip -d /tmp/app/
+rsync -av --delete /tmp/app/ /usr/share/nodeapp/
+
+common
+
+node_script
+
+# Remove temporary deployment.
+rm -r /tmp/app
+rm /usr/share/nodeapp/app.zip
