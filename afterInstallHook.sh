d599f8e5027fbb5f9accf2f476b6956d113066e7
juber-ivre <jnunes@immersivevreducation.com>
2021-04-06 13:46:38 +0100

    AfterInstall change

--- afterInstallHook.sh
+++ afterInstallHook.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#set -e
-  source /root/.bash_profile
-
+set -e
   rsync -av --delete /tmp/nodeapp/ /usr/share/nodeapp/
 	# Copy .env file
 	aws s3 cp s3://ait-deployment-dev/node-app/.env /usr/share/nodeapp/.env
@@ -9,7 +7,7 @@
   echo "------ Node Scripts ------"
 
 	# Run NPM
- cd /usr/share/nodeapp
+ cd /usr/share/nodeapp/
 	# Run NPM.
    npm i 
   npm run start
