3571c782059995789b762cdcfb2ee83fee54e75a
juber-ivre <jnunes@immersivevreducation.com>
2021-04-06 16:30:35 +0100

    AfterInstall change

--- afterInstallHook.sh
+++ afterInstallHook.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #set -e
 
-whoami
+#whoami
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   
+source /root/.bashrc
+
   rsync -av --delete /tmp/nodeapp/ /usr/share/nodeapp/
 	# Copy .env file
 	aws s3 cp s3://ait-deployment-dev/node-app/.env /usr/share/nodeapp/.env
