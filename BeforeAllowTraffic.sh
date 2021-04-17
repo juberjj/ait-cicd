ad100dddb9170e98282faa3edd0b60694a0f5b85
juber-ivre <jnunes@immersivevreducation.com>
2021-04-17 18:25:26 +0100

    Changes on script

--- BeforeAllowTraffic.sh
+++ BeforeAllowTraffic.sh
@@ -1,1 +1,3 @@
 curl -s -u admin:jnunes http://3.250.102.89:9000/api/qualitygates/project_status?projectKey=juberjj_ait-cicd
+
+if [ $(jq -r '.projectStatus.status' result.json) = ERROR ] ; then $CODEBUILD_BUILD_SUCCEEDING -eq 0 ;fi
